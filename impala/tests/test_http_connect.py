a8d1439d070c21024d62e8666dcbbb60f2805e00
Andrew Sherman <asherman@cloudera.com>
2020-05-07 15:04:43 -0700

    Add HTTP code handling to THttpClient (#380)
    Before this change Impyla is not checking HTTP return codes when
    using the hs2-http protocol. Impyla is sending a request message
    (e.g. send_CloseOperation) but the HTTP call to send this message may
    fail. This will result in a failure when reading the reply (e.g. in
    recv_CloseOperation) as there is no reply data to read. This will
    typically result in an 'EOFError'.
    
    In code that overrides THttpClient.flush(), check the HTTP code that is
    returned after the HTTP call is made. If the code is not 1XX
    (informational response) or 2XX (successful) then throw an RPCException.
    
    This change does not contain any attempt to recover from an HTTP failures
    but it does allow the failure to be detected and a message to be
    printed.
    
    In future it may be possible to retry after certain HTTP errors.
    
    Testing:
    - Add a new test that tries to connect to an HTTP
      server that always returns a 503 error. Check that an appropriate
      Exception(FIXME) is thrown

--- impala/tests/test_http_connect.py
+++ impala/tests/test_http_connect.py
@@ -11,8 +11,50 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import SimpleHTTPServer
+import SocketServer
+import httplib
+import socket
+import threading
+from contextlib import closing
 
 import pytest
+
+@pytest.yield_fixture
+def http_503_server():
+  class RequestHandler503(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    """A custom http handler that checks for duplicate 'Host' headers from the most
+    recent http request, and always returns a 503 http code"""
+
+    def do_POST(self):
+      # The unfortunately named self.headers here is an instance of mimetools.Message that
+      # contains the request headers.
+      request_headers = self.headers.headers
+
+      # Ensure that only one 'Host' header is contained in the request before responding.
+      host_hdr_count = sum([header.startswith('Host:') for header in request_headers])
+      assert host_hdr_count == 1, "duplicate 'Host:' headers in %s" % request_headers
+
+      # Respond with 503.
+      self.send_response(code=httplib.SERVICE_UNAVAILABLE, message="Service Unavailable")
+
+  class TestHTTPServer503(object):
+    def __init__(self):
+      self.HOST = "localhost"
+      self.PORT = get_unused_port()
+      self.httpd = SocketServer.TCPServer((self.HOST, self.PORT), RequestHandler503)
+
+      self.http_server_thread = threading.Thread(target=self.httpd.serve_forever)
+      self.http_server_thread.start()
+
+  server = TestHTTPServer503()
+  yield server
+
+  # Cleanup after test.
+  if server.httpd is not None:
+    server.httpd.shutdown()
+  if server.http_server_thread is not None:
+    server.http_server_thread.join()
 
 from impala.dbapi import connect
 
@@ -23,3 +65,20 @@ class TestHttpConnect(object):
     cur.execute('select 1')
     rows = cur.fetchall()
     assert rows == [(1,)]
+
+  def test_http_interactions(self, http_503_server):
+    """Test interactions with the http server when using hs2-http protocol.
+    Check that XXX when the server returns a 503 error."""
+
+    con = connect("localhost", http_503_server.PORT, use_http_transport=True)
+    con.cursor()
+
+
+
+
+def get_unused_port():
+  """ Find an unused port http://stackoverflow.com/questions/1365265 """
+  with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+    s.bind(('', 0))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    return s.getsockname()[1]
